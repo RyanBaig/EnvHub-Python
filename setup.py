06b37e15a8bb40019d6bf31f2fd45dd4100c9af9
RyanGamingYT <rg373429@gmail.com>
2023-12-18 15:51:51 +0500

    added description

--- setup.py
+++ setup.py
@@ -4,4 +4,11 @@ setup(
     name='EnvHub',
     version='1.0',
     packages=find_packages(),
+    author="RyanBaig",
+    long_description="""
+    # EnvHub Python SDK
+    ## Hello, This is the **official** Python SDK for [EnvHub](https://envhub.ryanbaig.vercel.app).
+    """,
+    long_description_content_type="text/markdown",
+    url='https://github.com/RyanBaig/EnvHub-Python',
 )
