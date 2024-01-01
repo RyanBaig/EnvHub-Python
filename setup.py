d073b585c923d90e04a7728a973ea0033613d39a
RyanGamingYT <rg373429@gmail.com>
2024-01-01 22:03:08 +0500

    Added new version! (1.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EnvHub',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     author="RyanBaig",
     long_description="""
