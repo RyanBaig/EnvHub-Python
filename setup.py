90aeed814a8b116986a3af5ba296313672c7a58b
RyanGamingYT <rg373429@gmail.com>
2024-01-14 12:36:27 +0500

    added correct version name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EnvHub',
-    version='1.2',
+    version='1.3',
     packages=find_packages(),
     author="RyanBaig",
     long_description="""
